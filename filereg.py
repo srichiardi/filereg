d7e199b8dd0206c1a6c35f993b6bd1009dc8e371
stefano <s.richiardi@gmail.com>
2015-09-19 15:58:47 +0100

    file extraction improvements

--- filereg.py
+++ filereg.py
@@ -10,10 +10,23 @@ class FileReg:
     def __init__(self):
         
         self.extractDict = {}
-        self.regexDict = {}
+        self.regexDict = {
+                          'Bank_Name' : { re.compile() : re.compile() },
+                          'Bank_SortCode' : { re.compile() : re.compile() },
+                          'Bank_Acct_Nr' : { re.compile() : re.compile() },
+                          'Bank_SwiftBic' : { re.compile() : re.compile() },
+                          'Bank_IBAN' : { re.compile() : re.compile() },
+                          'Bank_Beneficiary' : { re.compile() : re.compile() },
+                          'Phone_Nr' : { re.compile() : re.compile() },
+                          'Fax_Nr' : { re.compile() : re.compile() },
+                          'Email_From' : { re.compile() : re.compile() },
+                          'Email_To' : { re.compile() : re.compile() },
+                          'Email' : { re.compile() : re.compile() }
+                          }
         
         
     def loadRegexes(self, regList):
+        ''' adding custom regexes to default '''
 
         for line in regList:
             title, key, value = line.split(':::')
@@ -29,21 +42,12 @@ class FileReg:
         for f in filesList:
             currFile = open(f, 'r', newline='')
             
-            allLines = []
-            
-            # move file in a list
-            for line in currFile:
-                allLines.append(line)
-            
-            currFile.close()
-            
             # loop settings
             hdrSet = False
             self.extractDict[f] = { key : [] for key in self.regexDict.keys() }
             
-            # extraction loop (assumption: no more than one value per line)
-            for line in allLines:
-            
+            # assuming only one key, value or key-value pair per line, more values on same line will be lost
+            for line in currFile:
                 # find header, but skip this step if header already exists
                 for regType in self.regexDict.keys():
                     if self.regexDict[regType][0].match(line):
@@ -55,12 +59,16 @@ class FileReg:
                 if hdrSet and failCount < failLimit:
                     if self.regexDict[hdr][1].match(line):
                         value = self.regexDict[hdr][1].match(line).group()
-                        self.extractDict[f][hdr].extend(value)
+                        # add only if unique values, avoid duplicates
+                        if value not in self.extractDict[f][hdr]:
+                            self.extractDict[f][hdr].extend(value)
                         failCount = 0 # reset failcount after each positive match
                     else:
                         failCount += 1 # increase failcount after each negative match
                 else:
                     hdrSet = False # stop looking for header value after too many failcounts
+            
+            currFile.close()
 
     
     def exportToCsv(self,folderPath):
